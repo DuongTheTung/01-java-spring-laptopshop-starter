cfb946f421deb964197300fa36a1501131fed682
DuongTheTung <ankdbadao@gmail.com>
2026-02-03 12:26:42 +0700

    Update pom.xml and application.properties

--- src/main/java/com/example/laptopshop/HelloController.java
+++ src/main/java/com/example/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Sphxdzfhring Boot!";
+        return "Hello World from Boot!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin";
     }
 }
